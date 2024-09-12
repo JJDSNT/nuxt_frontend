bd3cd9cd7aa1c6784c5aa936aa242fbc28dff26a
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-12 14:34:35 -0300

    Add header and footer to app.vue

--- app.vue
+++ app.vue
@@ -1,7 +1,18 @@
 <template>
   <div>
     <NuxtRouteAnnouncer />
-    <EstadoCidadeSelect />
+    <header>
+      <h1>Minha Aplicação</h1>
+      <!-- Links de navegação -->
+      <nav>
+        <NuxtLink to="/">Home</NuxtLink>
+        <NuxtLink to="/indicadores">Pesquisar Indicadores</NuxtLink>
+      </nav>
+    </header>
+    <NuxtPage />
+    <footer>
+      <p>&copy; 2024 Minha Aplicação</p>
+    </footer>
   </div>
 </template>
 

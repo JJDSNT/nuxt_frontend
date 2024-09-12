8c5e9291c93631687d1ed9a775a433cc0441b4fd
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-12 20:42:45 -0300

    Refactor Footer component to include Nuxt(Vue) in the dedication message

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,7 @@
     <footer class="mt-10 flex flex-col items-center justify-center text-center" style="padding-bottom: 25px;">
         <hr class="w-1/3 my-4 border-gray-300" />
         <p class="mb-2">
-            Desenvolvido com <span role="img" aria-label="Coração">❤️</span> dedicação.
+            Desenvolvido com <span role="img" aria-label="Coração">❤️</span> dedicação e Nuxt(Vue).
         </p>
         <div class="flex items-center">
             <span style="margin-right: 5px;">

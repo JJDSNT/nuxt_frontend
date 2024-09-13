0e9b05e92a29c20fbb5ae46cb197b5a8a4ddc1de
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-13 14:19:39 -0300

    Refactor index.vue to update meta tags

--- pages/index.vue
+++ pages/index.vue
@@ -4,4 +4,11 @@
 
 <script setup>
 import Dashboard from '~/components/Dashboard.vue';
+
+useHead({
+  title: 'ObservaTudo',
+  meta: [
+    { name: 'description', content: 'Dashboard de indicadores cívicos' },
+  ],
+})
 </script>

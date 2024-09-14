a7d986b14fca091e7d997b4713699bd78aadb12d
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-14 18:06:21 -0300

    Refactor useInfoStore.ts to update state properties and getters

--- stores/useInfoStore.ts
+++ stores/useInfoStore.ts
@@ -1,11 +1,15 @@
 import { defineStore } from "pinia";
 
 // Definição das interfaces
+export interface Pais {
+  codigo: number;
+  nome: string;
+}
+
 export interface Estado {
   codigo: number;
   nome: string;
   sigla: string;
-  cidades: Cidade[];
 }
 
 export interface Cidade {
@@ -36,27 +40,29 @@ export enum Eixos {
 
 export const useInfoStore = defineStore("info", {
   state: () => ({
-    cidadeSelecionada: null as Cidade["codigo"] | null,
-    estadoSelecionado: null as Estado["codigo"] | null,
     eixoSelecionado: null as Eixos | null,
+    paisSelecionado: null as Pais | null,
+    estadoSelecionado: null as Estado | null, // Objeto completo de Estado, sem cidades
+    cidadeSelecionada: null as Cidade | null, // Objeto completo de Cidade
   }),
   getters: {
-    // Retorna o eixo selecionado diretamente
-    eixoCod: (state) => state.eixoSelecionado,
-  
-    // Retorna o estado selecionado
-    codigoEstadoSelecionado: (state) => state.estadoSelecionado,
+    // Getter para retornar nome do estado diretamente
+    estadoNome: (state) => state.estadoSelecionado?.nome || 'Não informado',
+    // Getter para retornar nome da cidade diretamente
+    cidadeNome: (state) => state.cidadeSelecionada?.nome || 'Não informado',
   },
   actions: {
-    setCidade(cidadeCodigo: Cidade["codigo"] | null) {
-      this.cidadeSelecionada = cidadeCodigo;
-      // Aqui não há inferência de estado, você pode atualizar o estado em outro local
-    },
     setEixo(eixo: Eixos | null) {
       this.eixoSelecionado = eixo;
     },
-    setEstado(estado: Estado["codigo"] | null) {
+    setPais(pais: Pais | null) {
+      this.paisSelecionado = pais;
+    },
+    setEstado(estado: Estado | null) {
       this.estadoSelecionado = estado;
+    },
+    setCidade(cidade: Cidade | null) {
+      this.cidadeSelecionada = cidade;
     },
   },
 });

7178d2ed98f1a43a671f0aeeec12f7cdd1d180e5
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 19:46:23 -0300

    Update nuxt.config.ts to add @nuxt/test-utils/module to modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,6 +5,6 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   devtools: { enabled: true },
-  modules: ['@nuxtjs/tailwindcss'],
+  modules: ['@nuxtjs/tailwindcss','@nuxt/test-utils/module'],
   plugins: ['~/plugins/backendCheck.js']
 })

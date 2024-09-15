19c70f7a78ca7661d5834645b0bcc27f005fc30e
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 19:46:29 -0300

    Add vitest.config.ts for Nuxt test configuration

---
+++ vitest.config.ts
@@ -0,0 +1,10 @@
+import { fileURLToPath } from 'node:url'
+import { defineVitestConfig } from '@nuxt/test-utils/config'
+
+export default defineVitestConfig({
+    test: {
+        environment: 'nuxt',
+      },
+})
+
+

79aaf041152f457f652bd0ef019c15d103e43abf
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 16:01:04 -0300

    Refactor Navbar.vue to use button element instead of div for dropdown trigger

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -3,21 +3,26 @@
         <div class="navbar bg-primary text-primary-content">
             <div class="navbar-start">
                 <div class="dropdown">
-                    <div tabindex="0" role="button" class="btn btn-ghost btn-circle">
+                    <!-- Alterado para button -->
+                    <button class="btn btn-ghost btn-circle">
                         <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24"
                             stroke="currentColor">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M4 6h16M4 12h16M4 18h7" />
                         </svg>
-                    </div>
-                    <ul tabindex="0"
-                        class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
-                        <li><NuxtLink to="/">Home</NuxtLink></li>
-                        <li><NuxtLink to="/indicadores">Pesquisar</NuxtLink></li>
+                    </button>
+                    <ul class="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+                        <li>
+                            <NuxtLink to="/">Home</NuxtLink>
+                        </li>
+                        <li>
+                            <NuxtLink to="/indicadores">Pesquisar</NuxtLink>
+                        </li>
                         <li><a>About</a></li>
                     </ul>
                 </div>
             </div>
+
             <div class="navbar-center">
                 <a class="btn btn-ghost text-xl">ObservaTudo</a>
             </div>

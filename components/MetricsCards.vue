df10c732beda047e2a325e0d2b450fdca70bd51d
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 02:28:34 -0300

    Refactor MetricsCards.vue to display selected location names from API

--- components/MetricsCards.vue
+++ components/MetricsCards.vue
@@ -10,25 +10,28 @@
 
     <!-- Indicadores do País -->
     <div v-if="indicadoresPais.length > 0">
-      <h4 class="text-lg font-semibold mt-4">Indicadores do País</h4>
+      <h4 class="text-lg font-semibold mt-4">Indicadores do País {{ paisNomeAPI }}</h4>
       <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
-        <MetricsCard v-for="indicador in indicadoresPais" :key="indicador.nomeIndicador" :indicador="indicador" :selectedLocalidade="paisNome" />
+        <MetricsCard v-for="indicador in indicadoresPais" :key="indicador.nomeIndicador" :indicador="indicador"
+          :selectedLocalidade="paisNome" />
       </div>
     </div>
 
     <!-- Indicadores do Estado -->
     <div v-if="indicadoresEstado.length > 0">
-      <h4 class="text-lg font-semibold mt-4">Indicadores do Estado</h4>
+      <h4 class="text-lg font-semibold mt-4">Indicadores do Estado {{ estadoNomeAPI }}</h4>
       <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
-        <MetricsCard v-for="indicador in indicadoresEstado" :key="indicador.nomeIndicador" :indicador="indicador" :selectedLocalidade="estadoNome" />
+        <MetricsCard v-for="indicador in indicadoresEstado" :key="indicador.nomeIndicador" :indicador="indicador"
+          :selectedLocalidade="estadoNome" />
       </div>
     </div>
 
     <!-- Indicadores da Cidade -->
     <div v-if="indicadoresCidade.length > 0">
-      <h4 class="text-lg font-semibold mt-4">Indicadores da Cidade</h4>
+      <h4 class="text-lg font-semibold mt-4">Indicadores da Cidade {{ cidadeNomeAPI }}</h4>
       <div class="grid grid-cols-1 sm:grid-cols-4 gap-4">
-        <MetricsCard v-for="indicador in indicadoresCidade" :key="indicador.nomeIndicador" :indicador="indicador" :selectedLocalidade="cidadeNome" />
+        <MetricsCard v-for="indicador in indicadoresCidade" :key="indicador.nomeIndicador" :indicador="indicador"
+          :selectedLocalidade="cidadeNome" />
       </div>
     </div>
 
@@ -67,6 +70,10 @@ const indicadoresPais = ref<Indicador[]>([]);
 const indicadoresEstado = ref<Indicador[]>([]);
 const indicadoresCidade = ref<Indicador[]>([]);
 
+const paisNomeAPI = ref('Não informado');
+const estadoNomeAPI = ref('Não informado');
+const cidadeNomeAPI = ref('Não informado');
+
 // Computed para obter o eixo selecionado
 const eixoSelecionado = computed(() => store.eixoSelecionado);
 
@@ -80,10 +87,15 @@ async function carregarIndicadores(cidadeId: number) {
       console.log('Indicadores carregados com sucesso:', response.data);
       const data = response.data;
 
+      // Computed para obter o nome do país, estado e cidade da API
+      paisNomeAPI.value = data.paisNome || 'Não informado';
+      estadoNomeAPI.value = data.estadoNome || 'Não informado';
+      cidadeNomeAPI.value = data.cidadeNome || 'Não informado';
+
       // Preenche os arrays de indicadores (somente os indicadores)
-      indicadoresPais.value = data.indicadoresPais || [];
-      indicadoresEstado.value = data.indicadoresEstado || [];
-      indicadoresCidade.value = data.indicadoresCidade || [];
+      indicadoresPais.value = (data.indicadoresPais || []) as Indicador[];
+      indicadoresEstado.value = (data.indicadoresEstado || []) as Indicador[];
+      indicadoresCidade.value = (data.indicadoresCidade || []) as Indicador[];
     } else {
       console.error('Erro na resposta ao carregar indicadores:', response.status);
     }

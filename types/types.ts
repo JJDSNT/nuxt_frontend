f48700f4ca8366e75a7438cc993f11740fadd9a1
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 16:30:42 -0300

    Refactor types/types.ts to add interfaces for countries, states, cities, indicators, and values

---
+++ types/types.ts
@@ -0,0 +1,44 @@
+// Definição das interfaces
+export interface Pais {
+  codigo: number;
+  nome: string;
+}
+
+export interface Estado {
+  codigo: number;
+  nome: string;
+  sigla: string;
+  cidades?: Cidade[];
+}
+
+export interface Cidade {
+  codigo: number;
+  nome: string;
+  codigoEstado: number;
+  capital?: boolean;
+}
+
+export interface Indicador {
+  fonte: string | null;
+  id: number;
+  nomeIndicador: string;
+  descricao: string;
+  eixo: string | null;
+  valores?: Valor[];
+}
+
+export interface Valor {
+  data: string;
+  valor: number;
+}
+
+export enum Eixos {
+  SAUDE = 1,
+  EDUCACAO = 2,
+  ASSISTENCIA_SOCIAL = 3,
+  SEGURANCA = 4,
+  MEIO_AMBIENTE = 5,
+  ECONOMIA = 6,
+  GOVERNANCA = 7,
+  PERSONALIZADO = 8,
+}

b0f49c74401874ceb10461d641c4f815313a5fcd
Jaime Dias <jaimejosediasnt@gmail.com>
2024-09-15 19:58:40 -0300

    Update vitest.config.ts to configure Vite test environment and coverage reports

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,9 +2,15 @@ import { fileURLToPath } from 'node:url'
 import { defineVitestConfig } from '@nuxt/test-utils/config'
 
 export default defineVitestConfig({
-    test: {
-        environment: 'nuxt',
-      },
+  test: {
+    globals: true,
+    environment: 'happy-dom',
+    coverage: {
+      provider: 'v8',
+      reportsDirectory: './coverage', // Diretório para armazenar os relatórios de cobertura
+      reporter: ['text', 'json-summary', 'lcov'], // Formatos dos relatórios
+    },
+  },
 })
 
 
